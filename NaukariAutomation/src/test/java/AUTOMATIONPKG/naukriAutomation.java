12c32e902b36ca27ce61cd50a913e957880d8dfc
PRATIK <PRATIK@PRATIK-PC>
2022-10-04 23:16:21 +0530

    Test comment

---
+++ NaukariAutomation/src/test/java/AUTOMATIONPKG/naukriAutomation.java
@@ -0,0 +1,10 @@
+package AUTOMATIONPKG;
+
+public class naukriAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello Word");
+	}
+
+}
